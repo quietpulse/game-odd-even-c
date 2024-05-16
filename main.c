bf02157b0fd93526cf7d5ebf7f230880d5f03c77
aiaman <amanme9285@gmail.com>
2024-05-16 09:22:07 +0530

    Tweaks : Removed childishness 😅

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 // Calling functions on top to remove warnings!
-// this is just a testing comment
-void amanFunction(int randomNum, int choice, char* name, int amanScore, int userScore);
-void userFunction(int randomNum, int choice, char* name, int userScore, int amanScore);
+// This is just a testing comment
+void aiFunction(int randomNum, int choice, char* name, int aiScore, int userScore);
+void userFunction(int randomNum, int choice, char* name, int userScore, int aiScore);
 void displayInstructions();
 
 int main() {
@@ -15,11 +15,11 @@ int main() {
     int randomNumMain = (rand() % 2) + 1; // Saving random value for choice 1 or 2 on main menu.
     int choice;
     int userScore = 0;
-    int amanScore = 0;
-    int randomNum; // This value is used in amanFunction and userFunction.
+    int aiScore = 0;
+    int randomNum; // This value is used in aiFunction and userFunction.
 
-    printf("\nWelcome to the Odd-Even Game Main Menu!\n");
-    printf("I am Aman, your opponent in this game.\n\n");
+    printf("\nWelcome to the Number Guessing Game Main Menu!\n");
+    printf("I am Ai, your opponent in this game.\n\n");
     printf("Please enter your first name: ");
     scanf("%s", name);
     printf("\n\nOkay, %s, shall we start the game?\n", name);
@@ -36,12 +36,12 @@ int main() {
             case 1:
                 printf("%s is the main player.\n", name);
                 // Calling user input and output function.
-                userFunction(randomNum, choice, name, userScore, amanScore);
+                userFunction(randomNum, choice, name, userScore, aiScore);
                 break;
             case 2:
-                // Calling Aman's output function.
-                printf("\nAman is the main player.\n");
-                amanFunction(randomNum, choice, name, amanScore, userScore);
+                // Calling Ai's output function.
+                printf("\nAi is the main player.\n");
+                aiFunction(randomNum, choice, name, aiScore, userScore);
                 break;
             default:
                 printf("Error: Random value generated incorrectly!\nExiting the game.\n");
@@ -64,46 +64,46 @@ int main() {
 }
 
 // User input and output function
-void userFunction(int randomNum, int choice, char* name, int userScore, int amanScore) {
+void userFunction(int randomNum, int choice, char* name, int userScore, int aiScore) {
     // User's turn.
     do {
         randomNum = (rand() % 10) + 1;
         printf("\nEnter your number: ");
         scanf("%d", &choice);
-        printf("\nAman = %d\n", randomNum);
+        printf("\nAi = %d\n", randomNum);
         printf("%s = %d\n", name, choice);
         userScore = userScore + (randomNum + choice);
     } while (randomNum != choice);
 
     printf("\n\n%s's total score is \"%d\".\n", name, userScore);
 
-    // Aman's turn.
+    // Ai's turn.
     do {
-        printf("\n\n\nNow Aman is the main player.");
+        printf("\n\n\nNow Ai is the main player.");
         randomNum = (rand() % 10) + 1;
         printf("\nEnter your number: ");
         scanf("%d", &choice);
-        printf("\nAman = %d\n", randomNum);
+        printf("\nAi = %d\n", randomNum);
         printf("%s = %d\n", name, choice);
-        amanScore = amanScore + (randomNum + choice);
+        aiScore = aiScore + (randomNum + choice);
     } while (randomNum != choice);
 
-    printf("\n\nAman's total score is \"%d\".\n", amanScore);
+    printf("\n\nAi's total score is \"%d\".\n", aiScore);
 
     // Result if user wins.
-    if (amanScore < userScore) {
+    if (aiScore < userScore) {
         printf("\n\n\n/==========================================================/");
-        printf("\n\nAman loses!\n");
-        printf("%s loses! :(\n", name);
-        printf("\nTotal score of Aman: %d", amanScore);
+        printf("\n\nAi loses!\n");
+        printf("%s wins! :)\n", name);
+        printf("\nTotal score of Ai: %d", aiScore);
         printf("\nTotal score of %s: %d\n\n", name, userScore);
     }
-    // Result if Aman wins.
-    else if (amanScore > userScore) {
+    // Result if Ai wins.
+    else if (aiScore > userScore) {
         printf("\n\n\n/==========================================================/");
-        printf("\n\nAman Wins!!\n");
+        printf("\n\nAi Wins!!\n");
         printf("%s loses! :(\n", name);
-        printf("\nTotal score of Aman: %d", amanScore);
+        printf("\nTotal score of Ai: %d", aiScore);
         printf("\nTotal score of %s: %d\n\n", name, userScore);
     }
     else {
@@ -112,19 +112,19 @@ void userFunction(int randomNum, int choice, char* name, int userScore, int aman
     }
 }
 
-// Aman's generated outputs function
-void amanFunction(int randomNum, int choice, char* name, int amanScore, int userScore) {
-    // Aman's turn.
+// Ai's generated outputs function
+void aiFunction(int randomNum, int choice, char* name, int aiScore, int userScore) {
+    // Ai's turn.
     do {
         randomNum = (rand() % 10) + 1;
         printf("\nEnter your number: ");
         scanf("%d", &choice);
-        printf("\nAman = %d\n", randomNum);
+        printf("\nAi = %d\n", randomNum);
         printf("%s = %d\n", name, choice);
-        amanScore = amanScore + (randomNum + choice);
+        aiScore = aiScore + (randomNum + choice);
     } while (randomNum != choice);
 
-    printf("\n\nAman's total score is %d.\n", amanScore);
+    printf("\n\nAi's total score is %d.\n", aiScore);
     printf("\n\n\nNow %s is the main player.", name);
 
     // User's turn.
@@ -132,27 +132,27 @@ void amanFunction(int randomNum, int choice, char* name, int amanScore, int user
         randomNum = (rand() % 10) + 1;
         printf("\nEnter your number: ");
         scanf("%d", &choice);
-        printf("\nAman = %d\n", randomNum);
+        printf("\nAi = %d\n", randomNum);
         printf("%s = %d\n", name, choice);
         userScore = userScore + (randomNum + choice);
     } while (randomNum != choice);
 
     printf("\n\n%s's total score is %d.\n", name, userScore);
 
-    // Result if Aman wins.
-    if (amanScore < userScore) {
+    // Result if Ai wins.
+    if (aiScore < userScore) {
         printf("\n/==========================================================/");
-        printf("\nAman loses!\n");
-        printf("%s loses! :(\n", name);
-        printf("\nTotal score of Aman: %d", amanScore);
+        printf("\nAi loses!\n");
+        printf("%s wins! :)\n", name);
+        printf("\nTotal score of Ai: %d", aiScore);
         printf("\nTotal score of %s: %d\n\n", name, userScore);
     }
     // Result if user wins.
-    else if (amanScore > userScore) {
+    else if (aiScore > userScore) {
         printf("\n/==========================================================/");
-        printf("\nAman Wins!!\n");
+        printf("\nAi Wins!!\n");
         printf("%s loses! :(\n", name);
-        printf("\nTotal score of Aman: %d", amanScore);
+        printf("\nTotal score of Ai: %d", aiScore);
         printf("\nTotal score of %s: %d\n\n", name, userScore);
     }
     else {
@@ -163,10 +163,10 @@ void amanFunction(int randomNum, int choice, char* name, int amanScore, int user
 
 // Display game instructions.
 void displayInstructions() {
-    printf("1. The game is played between You and Aman.\n");
-    printf("2. You and Aman take turns entering a number.\n");
+    printf("1. The game is played between You and Ai.\n");
+    printf("2. You and Ai take turns entering a number.\n");
     printf("3. A random number will also be generated by the computer for each turn.\n");
     printf("4. If your number matches the computer's number, you score a point.\n");
-    printf("5. The game continues until you or Aman matches the computer's number.\n");
+    printf("5. The game continues until you or Ai matches the computer's number.\n");
     printf("6. The player with the highest score wins.\n");
 }
